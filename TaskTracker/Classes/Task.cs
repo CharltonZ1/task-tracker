3810e19b0002a47b5e866f487879cc4413424e37
Charlton Adams <charltonadams38@gmail.com>
2025-01-26 11:57:23 +0200

    Add Task class and StatusEnum

---
+++ TaskTracker/Classes/Task.cs
@@ -0,0 +1,27 @@
+﻿namespace TaskTracker.Classes;
+
+public class Task
+{
+    private static int _id = 0;
+    public int Id { get; set; }
+    public string Description { get; set; }
+    public StatusEnum Status { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+
+    public Task(string desc)
+    {
+        Id = _id++;
+        Description = desc;
+        Status = StatusEnum.todo;
+        CreatedAt = DateTime.Now;
+        UpdatedAt = DateTime.Now;
+    }
+}
+
+public enum StatusEnum
+{
+    todo,
+    inProgress,
+    done
+}
